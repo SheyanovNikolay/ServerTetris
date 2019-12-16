7ce9d2bbf368945e218d8da491891e4fe8aff328
SheyanovNikolay <shnn1998@gmail.com>
2019-12-17 00:14:26 +0300

    Commit 2

--- server/Program.cs
+++ server/Program.cs
@@ -100,7 +100,6 @@ namespace server
             {
                 while (true)
                 {
-                    // loh
                     buffer = clientStream.Read(readBuffer, 0, readBuffer.Length);// чтение конекшена и готовности всех игроков
                     message = Encoding.Unicode.GetString(readBuffer, 0, buffer);
                     Console.WriteLine(name + ": " + (message == "1" ? "Подключился":"Готов играть"));
@@ -137,7 +136,7 @@ namespace server
                                 writeBuffer = Encoding.Unicode.GetBytes(ToString(map));
                                 clientStream.Write(writeBuffer, 0, writeBuffer.Length);
                                 RandomMatrix(map);
-                                Thread.Sleep(100);
+                                Thread.Sleep(300);
                             }
                     }
                 }
